8dd82bae0092ccb6ab559a507a0b4797253f5380
Dugina80 <alenka251280d@gmail.com>
2023-04-25 19:55:55 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const helenaDuhina = "Helena Duhina";
+const arinaDuhina = "Arina Duhina";
+const artemDuhin = "Artem Duhin";
+const ludaZozulya = "Luda Zozulya";
+const lidaYaremko = "lida Yaremko";
+
+//const 1name = "Helena Duhina";
+/*const arina Duhina = "Arina Duhina";
+const ім'я = "Artem Duhin";
+const 4 = "Luda Zozulya";
+const a = "lida Yaremko";*/
+
+const nameOne = "a";
+const name_One = "b";
+const $name = "c";
+
+const name = prompt ('What is you name');
+alert(`You are ${name}`);
+
+const year_Birth = prompt ('What is your year of birth?');
+const thisYear = "2023";
+const age = (thisYear - year_Birth);
+alert(`You are ${age}`);
+
+const side_Square  = prompt ('Enter the length of the side of the square');
+const perimeter_Sguare = (4*side_Square);
+alert(`perimeter of the square ${perimeter_Sguare}`)
+
+const circleRadius  = prompt ('Enter the radius of the circle');
+const area_circle = (3.14*(circleRadius**2));
+alert(`area of ​​a circle ${area_circle}`);
+
+const distance = prompt ('how many kilometers between two cities');
+const time = prompt ('How many hours does it take to get to the city');
+const speed = (distance / time);
+alert(`your speed should be ${speed}`);
+
+const dollar = prompt ('enter the amount of dollars');
+const exchangeRates = "0.91";
+const euro = (dollar * exchangeRates);
+alert(`euro ${euro}`);
